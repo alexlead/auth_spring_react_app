78a8b0d5e7ed2206ab14f3be05c29ac729286d58
Alexander Lead <a.lead@yandex.ru>
2024-03-15 10:13:21 +0300

    feat(client, server): react proj, spring base

---
+++ server/src/main/java/com/securityserver/study/domain/User.java
@@ -0,0 +1,13 @@
+package com.securityserver.study.domain;
+
+public class User {
+
+    private String id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String role = "ROLE_CUSTOMER";
+    private String mobile;
+
+
+}
